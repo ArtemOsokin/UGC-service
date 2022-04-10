bca5f7df8680cf471605332dc2a61ffd242b478d
Artem Osokin <aptem21@mail.ru>
2022-04-10 23:40:33 +0300

    fix research

--- event-api/research/vertica/test.py
+++ event-api/research/vertica/test.py
@@ -12,7 +12,7 @@ def test_insert():
     with vertica_python.connect(**dsl) as connection:
         cursor = connection.cursor()
         data = []
-        for _ in range(100):
+        for _ in range(10000):
             row = (
                 random.randint(1, 1500),
                 random.randint(1, 20000),
@@ -24,18 +24,39 @@ def test_insert():
 
 
 @time_run_func
-def test_select():
+def test_select_count():
     with vertica_python.connect(**dsl) as connection:
         cursor = connection.cursor()
         cursor.execute('SELECT COUNT(*) FROM test')
+
+
+@time_run_func
+def test_select_by_movie_mark():
+    with vertica_python.connect(**dsl) as connection:
+        cursor = connection.cursor()
         cursor.execute('SELECT movie_mark FROM test')
+
+
+@time_run_func
+def test_select_count_by_user_id_300():
+    with vertica_python.connect(**dsl) as connection:
+        cursor = connection.cursor()
         cursor.execute('SELECT * FROM test WHERE user_id=300')
+
+
+@time_run_func
+def test_select_sum():
+    with vertica_python.connect(**dsl) as connection:
+        cursor = connection.cursor()
         cursor.execute('SELECT SUM(movie_id) FROM test')
 
 
 def main():
     test_insert()
-    test_select()
+    test_select_count()
+    test_select_by_movie_mark()
+    test_select_count_by_user_id_300()
+    test_select_sum()
 
 
 if __name__ == '__main__':
